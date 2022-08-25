ebea0d0aa27327fe2afb85c1c1d9148b1b45300f
jbgoldman1104 <jbgoldman1104@gmail.com>
2022-08-24 21:25:39 -0800

    "updated on 2022-08-24"

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ require("./api/utils/admin-seed");
 const app = require("./app");
 
 /**
+ * Starting point of the application
+ * configuration file is initialized
+ * and server is started.
+ */
+app.listen(config.port, () => {
+  console.log("Server running on", config.port);
 });
 
 module.exports = app; // for testing
